942150d0f287d5c3474dce5ea70c0b1d2a9b39ff
Ashish Mathur <52169296+mathur-ashish@users.noreply.github.com>
2021-10-23 10:13:11 +0530

    Revised table structure

--- DB/DB_Build.sql
+++ DB/DB_Build.sql
@@ -1,24 +1,11 @@
-drop table if exists interviews;
-drop table if exists candidateskillmap;
-drop table if exists jdskillmap;
-drop table if exists candidateprofile;
-drop table if exists JDs;
-drop table if exists skills;
+DROP TABLE IF EXISTS JD;
+DROP TABLE IF EXISTS CandidateProfile;
 
-CREATE TABLE Skills (
-    SkillId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
-    TechnicalSoftSkillFlag CHAR(1),
-    InsTS DATETIME,
-    InsBy VARCHAR(100),
-    UpdTS DATETIME,
-    UpdBy VARCHAR(100)
-);
-
-CREATE TABLE JDs (
-    JDId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
-    Description VARCHAR(100),
+CREATE TABLE JD (
+    JDId INT AUTO_INCREMENT PRIMARY KEY,
+    JDName VARCHAR(500),
+    Skill_GoodToHave TEXT,
+    Skill_Mandatory TEXT,
     InsTS DATETIME,
     InsBy VARCHAR(100),
     UpdTS DATETIME,
@@ -26,70 +13,19 @@ CREATE TABLE JDs (
 );
 
 CREATE TABLE CandidateProfile (
-    CandidateProfileId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(100),
+    CandidateProfileId INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(500),
     EmailAddress VARCHAR(100),
-    MobileNumber VARCHAR(100),
+    MobileNumber VARCHAR(15),
     OverallMatchScore DOUBLE,
+    Skills TEXT,
+    TP1Comment TEXT,
+    TP2Comment VARCHAR(100),
+    Status VARCHAR(100),
+    IsJoined CHAR(1),
+    DueDate DATETIME,
     InsTS DATETIME,
     InsBy VARCHAR(100),
     UpdTS DATETIME,
     UpdBy VARCHAR(100)
-);
-
-CREATE TABLE JDSkillMap (
-    JDSkillMapId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    JDId INT,
-    SkillId INT,
-    MustHaveFlag CHAR(1),
-    InsTS DATETIME,
-    InsBy VARCHAR(100),
-    UpdTS DATETIME,
-    UpdBy VARCHAR(100),
-    FOREIGN KEY (JDId)
-        REFERENCES JDs (JDId),
-    FOREIGN KEY (SkillId)
-        REFERENCES Skills (SkillId)
-);
-
-
-CREATE TABLE CandidateSkillMap (
-    CandidateSkillMapId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    CandidateProfileId INT,
-    SkillId INT,
-    InsTS DATETIME,
-    InsBy VARCHAR(100),
-    UpdTS DATETIME,
-    UpdBy VARCHAR(100),
-    FOREIGN KEY (SkillId)
-        REFERENCES Skills (SkillId),
-    FOREIGN KEY (CandidateProfileId)
-        REFERENCES CandidateProfile (CandidateProfileId)
-);
-
-
-CREATE TABLE Interviews (
-    InterviewId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    CandidateProfileId INT,
-    JDId INT,
-    TP1DateTime DATETIME,
-    TP1Interviewer VARCHAR(100),
-    TP1Result VARCHAR(100),
-    TP2DateTime DATETIME,
-    TP2Interviewer VARCHAR(100),
-    TP2Result VARCHAR(100),
-    ManagerRoundDateTime DATETIME,
-    ManagerRoundInterviewer VARCHAR(100),
-    ManagerRoundResult VARCHAR(100),
-    Status VARCHAR(100),
-    IsJoined CHAR(1),
-    DueDate DATE,
-    InsTS DATETIME,
-    InsBy VARCHAR(100),
-    UpdTS DATETIME,
-    UpdBy VARCHAR(100),
-    FOREIGN KEY (JDId)
-        REFERENCES JDs (JDId),
-    FOREIGN KEY (CandidateProfileId)
-        REFERENCES CandidateProfile (CandidateProfileId)
 );
